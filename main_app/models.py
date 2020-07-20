c57787e3be219cf4f26338d52426bf94bc69c81a
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-07-20 16:21:53 -0400

    added validations

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,27 @@
 from django.db import models
+from datetime import *
 
 # Create your models here.
+class ShowManager(models.Manager):
+    def showValidator(self, postData):
+        error = {}
+        if len(postData['title']) < 2:
+            error['title'] = "You must include a title at least 2 characters long."
+        if len(postData['network']) < 3:
+            error['network'] = "You must include a network name at least 3 characters long."
+        if len(postData['desc']) > 0 and len(postData['desc']) < 10:
+            error['desc'] = "If you include a description, it must be at least 10 characters long."
+        if len(postData['release_date']) < 1:
+            error['release_date'] = "You must provide a release date."
+        # elif datetime.datetime(postData['release_date']) >= datetime.now():
+        #     error['release_date'] = "The release date you choose must be in the past."
+        return error
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=45)
     release_date = models.DateField()
-    desc = models.TextField()
+    desc = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = ShowManager()
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Show
+from django.contrib import messages
 
 # Create your views here.
 def root(request):
@@ -9,19 +10,26 @@ def shows(request):
     context = {
         'shows': Show.objects.all() 
     }
+    print(context['shows'].values())
     return render(request, 'shows.html', context)
 
 def new(request):
     return render(request, 'new_show.html')
 
 def addShow(request):
-    title = request.POST['title']
-    network = request.POST['network']
-    rel_date = request.POST['release_date']
-    desc = request.POST['desc']
-    Show.objects.create(title=title, network=network, release_date=rel_date, desc=desc)
-    newId = Show.objects.last()
-    return redirect('/shows/' + str(newId.id))
+    errors = Show.objects.showValidator(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        title = request.POST['title']
+        network = request.POST['network']
+        rel_date = request.POST['release_date']
+        desc = request.POST['desc']
+        Show.objects.create(title=title, network=network, release_date=rel_date, desc=desc)
+        newId = Show.objects.last()
+        return redirect('/shows/' + str(newId.id))
 
 def showDetails(request, show_id):
     context = {
@@ -35,15 +43,21 @@ def editShow(request, show_id):
             'show': Show.objects.get(id=show_id)
         }   
         return render(request, 'edit_show.html', context)
-    
+        
     if request.method == 'POST':
-        show = Show.objects.get(id=show_id)
-        show.title = request.POST['title']
-        show.network = request.POST['network']
-        show.release_date = request.POST['release_date']
-        show.desc = request.POST['desc']
-        show.save()
-        return redirect('/shows/' + str(show.id))
+        errors = Show.objects.showValidator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/shows/' + str(show_id) + '/edit')
+        else:
+            show = Show.objects.get(id=show_id)
+            show.title = request.POST['title']
+            show.network = request.POST['network']
+            show.release_date = request.POST['release_date']
+            show.desc = request.POST['desc']
+            show.save()
+            return redirect('/shows/' + str(show.id))
 
 def deleteShow(request, show_id):
     show = Show.objects.get(id=show_id)
